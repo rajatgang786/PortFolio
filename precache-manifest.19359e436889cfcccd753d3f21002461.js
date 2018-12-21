3d8e12d287067d9dad78f588f5d823860d4648b3
Rajat Gang <rajatgang786@gmail.com>
2018-12-21 12:45:55 +0530

    Updates

---
+++ precache-manifest.19359e436889cfcccd753d3f21002461.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64c8254555ff6651d4ba23672690eb34",
+    "url": "/portfolio/static/media/rajat_profile.64c82545.jpg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "1cd2fe364899239237f3",
+    "url": "/portfolio/static/js/main.1cd2fe36.chunk.js"
+  },
+  {
+    "revision": "06b2d626aa4a0f6b7d44",
+    "url": "/portfolio/static/js/1.06b2d626.chunk.js"
+  },
+  {
+    "revision": "1cd2fe364899239237f3",
+    "url": "/portfolio/static/css/main.fb305c33.chunk.css"
+  },
+  {
+    "revision": "9607f3dd1bbff17230881d69df4574a5",
+    "url": "/portfolio/index.html"
+  }
+];
